f330a1ba23bfb14f2415dae5c67458d722ced2bb
Dennis Li <dennis.li@skyscanner.net>
2025-07-24 13:22:52 +0100

    Initial Commit

---
+++ branchsdk demo/branchsdk_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  branchsdk_demoApp.swift
+//  branchsdk demo
+//
+//  Created by Dennis Li on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct branchsdk_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
